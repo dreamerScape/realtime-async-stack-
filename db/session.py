1b95f83666ef5c772f881ead0105ce73beef556b
Makar <byreymakar@gmail.com>
2025-11-09 17:18:05 +0200

    feat: Implement async SQLAlchemy engine and session

---
+++ db/session.py
@@ -0,0 +1,21 @@
+import os
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
+
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine= create_async_engine(url=DATABASE_URL, echo=True)
+
+SesionLocal = sessionmaker(
+     
+    bind=engine,
+    class_=AsyncSession
+    
+)
+async def get_db_session():
+    async with SesionLocal() as session:
+        try:
+            yield session
+        finally:
+            await session.close()
