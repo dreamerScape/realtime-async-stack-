519e22f2c309f11efd2b63cc694a8cde6c8b6874
Makar <byreymakar@gmail.com>
2025-11-10 01:18:31 +0200

    feat(repo): Implement PollRepository for create_poll

---
+++ repositories/poll_repo.py
@@ -0,0 +1,23 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from models.poll import Poll, Option
+from shemas.poll import PollCreate, OptionCreate
+
+async def create_poll(db: AsyncSession, poll_data: PollCreate) -> Poll:
+    db_poll = Poll(question=poll_data.question)
+    
+    db.add(db_poll)
+    
+    await db.flush()
+    
+    db_options = [
+        Option(text=opt.text, poll_id=db_poll.id)
+        for opt in poll_data.options
+    ]
+    
+    db.add_all(db_options)
+    
+    await db.commit()
+    
+    await db.refresh(db_poll)
+    
+    return db_poll
