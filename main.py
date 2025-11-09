06f449733345a697f73f0f4baefc6e68dee9b080
Makar <byreymakar@gmail.com>
2025-11-09 17:20:43 +0200

    test: Verify db connection in root endpoint

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from db.session import get_db_session
+
 app = FastAPI(title="Real-time Async Stack")
 
 @app.get("/")
-def read_root():
-    return {"Hello": "From Docker!"}
+def read_root(db: AsyncSession = Depends(get_db_session)):
+    return {"status": "ok", "db_connection": "successful"}
