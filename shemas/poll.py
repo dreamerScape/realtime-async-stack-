ab49bc46a62b423bd0ce9552dbaad84f330f08f2
Makar <byreymakar@gmail.com>
2025-11-10 01:09:40 +0200

    feat: Add Pydantic schemas for Poll and Option

---
+++ shemas/poll.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel, ConfigDict
+from typing import List
+
+class OptionCreate(BaseModel):
+    text: str
+    
+class Option(OptionCreate):
+    id: int
+    votes: int
+    poll_id: int
+
+    model_config = ConfigDict(from_attributes=True)
+    
+    
+class PollCreate(BaseModel):
+    question: str
+    options: List[OptionCreate]
+    
+
+class Poll(BaseModel):
+    id: int
+    question: str
+    options: List[Option] = []
+
+    model_config = ConfigDict(from_attributes=True)
