9e6888f2d0b661c4b212a854d80298bc80bb8052
Makar <byreymakar@gmail.com>
2025-11-10 02:00:29 +0200

    feat(api): Add POST /polls endpoint to create polls

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
-from fastapi import FastAPI,Depends
+from fastapi import FastAPI,Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
-
+from sqlalchemy.exc import SQLAlchemyError
 from db.session import get_db_session
-from models import poll
+
+from shemas.poll import PollCreate, Poll as PollSchema
+from services import poll_service
 app = FastAPI(title="Real-time Async Stack")
 
-@app.get("/")
-def read_root(db: AsyncSession = Depends(get_db_session)):
-    return {"status": "ok", "db_connection": "successful"}
+# @app.get("/")
+# def read_root(db: AsyncSession = Depends(get_db_session)):
+#     return {"status": "ok", "db_connection": "successful"}
+
+@app.post("/polls", response_model=PollSchema)
+async def create_poll_endpoint(
+    poll_data: PollCreate,
+    db: AsyncSession = Depends(get_db_session)
+):
+    try:
+        new_poll = await poll_service.create_new_poll(db, poll_data)
+        return new_poll
+    except SQLAlchemyError as e:
+        await db.rollback()
+        raise HTTPException(status_code=400, detail=f"Eror creating poll, {str(e)}") 
+    except Exception as e:
+        await db.rollback()
+        raise HTTPException(status_code=500, detail=f"Internal Server Error {str(e)}")
+    
