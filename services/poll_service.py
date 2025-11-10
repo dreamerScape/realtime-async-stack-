a0c093b0fd76da9bdbfad1f07c6f7d7a3d8ae066
Makar <byreymakar@gmail.com>
2025-11-10 23:04:28 +0200

    refactor(service): Update PollService to use BrokerService

--- services/poll_service.py
+++ services/poll_service.py
@@ -3,6 +3,9 @@ from shemas.poll import PollCreate, VoteCreate
 from models.poll import Poll
 from repositories import poll_repo
 
+from redis.asyncio import Redis
+from services import broker_service
+
 async def create_new_poll(db: AsyncSession, poll_data: PollCreate) -> Poll:
     #.... buisness logic can be added here ...
     return await poll_repo.create_poll(db, poll_data)
@@ -11,13 +14,13 @@ async def get_poll(db: AsyncSession, poll_id: int) -> Poll:
     #.... buisness logic can be added here ...
     return await poll_repo.get_poll_by_id(db, poll_id)
 
-async def add_vote(db: AsyncSession, poll_id: int, vote_data: VoteCreate):
+async def add_vote(redis: Redis, poll_id: int, vote_data: VoteCreate) -> None:
     #.... buisness logic can be added here ...
     
-    updated_opiton = await poll_repo.add_vote_to_option(
-        db=db,
+    await broker_service.publish_vote_event(
+        redis=redis,
         poll_id=poll_id,
         option_id=vote_data.option_id
     )
     
-    return updated_opiton
+    return None
