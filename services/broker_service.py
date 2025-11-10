f17b073667ca3926e7ac80f8bc3cfcae9cd841ae
Makar <byreymakar@gmail.com>
2025-11-10 23:02:01 +0200

    feat(service): Add broker_service to publish votes to Redis Stream

---
+++ services/broker_service.py
@@ -0,0 +1,16 @@
+from redis.asyncio import Redis
+import json
+
+VOTE_STREAM_NAME = "voye_events"
+
+async def publish_vote_event(poll_id: int, option_id: int, redis: Redis):
+    
+    event_data = {
+        "poll_id": str(poll_id),
+        "option_id": str(option_id)
+    }
+    
+    await redis.xadd(
+        VOTE_STREAM_NAME,
+        {"event_data": json.dumps(event_data)}
+    )
