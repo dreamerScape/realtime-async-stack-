429067a49331a3487acad52504a9b11061cc98df
Makar <byreymakar@gmail.com>
2025-11-11 16:27:12 +0200

    feat(db): Add synchronous database engine and session management

--- db/session.py
+++ db/session.py
@@ -1,11 +1,23 @@
 import os
+from sqlalchemy import create_engine
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 
 
 DATABASE_URL = os.getenv("ASYNC_DATABASE_URL")
 
+
+
 engine= create_async_engine(url=DATABASE_URL, echo=True)
+
+
+SYNC_DATABASE_URL = os.environ.get("SYNC_DATABASE_URL")
+sync_engine = create_engine(SYNC_DATABASE_URL)
+SyncSessionLocal = sessionmaker(
+    autocommit=False, 
+    autoflush=False, 
+    bind=sync_engine
+)
 
 SesionLocal = sessionmaker(
      
