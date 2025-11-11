bc4e1d3ef90eb923583329bd8766ec73f34da6f0
Makar <byreymakar@gmail.com>
2025-11-11 15:38:30 +0200

    feat(worker): Worker now publishes to Pub/Sub on success

--- worker.py
+++ worker.py
@@ -10,7 +10,7 @@ from services import poll_service, cashe_service, broker_service
 
 from repositories import poll_repo
 
-from shemas.poll import VoteCreate
+from shemas.poll import VoteCreate, Option as OptionSchema
 
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
@@ -19,7 +19,7 @@ STREAM_NAME = broker_service.VOTE_STREAM_NAME
 
 GROUP_NAME = "poll_workers"
 
-async def procees_vote_event(event_data: dict):
+async def procees_vote_event(redis: Redis, event_data: dict):
     try:
         data = json.loads(event_data['event_data'])
         poll_id = int(data['poll_id'])
@@ -37,6 +37,12 @@ async def procees_vote_event(event_data: dict):
         async with Redis(connection_pool=redis_pool) as redis:
             await cashe_service.clear_poll_cashe(poll_id, redis)
         
+        option_shema = OptionSchema.model_validate(updated_option)
+        await broker_service.publish_real_time_update(
+            redis=redis,
+            poll_id=poll_id,
+            message_data=option_shema.model_dump_json()
+        )
         log.info(f"Successfully processed vote for Poll {poll_id}, Option {option_id}")   
         
     except Exception as e:
